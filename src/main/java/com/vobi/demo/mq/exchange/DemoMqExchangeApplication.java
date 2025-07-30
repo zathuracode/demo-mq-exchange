6c3f78d5160ab6f166f8cdb83f527900e0cecb87
Diego Armando Gomez <dgomez@vortexbird.com>
2025-07-30 08:06:40 -0500

    first commit

---
+++ src/main/java/com/vobi/demo/mq/exchange/DemoMqExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.vobi.demo.mq.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMqExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMqExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vobi/demo/mq/exchange/DemoMqExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vobi.demo.mq.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMqExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
